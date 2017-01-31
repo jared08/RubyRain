7f6a2e3414fa9d22fb6935baeadd96cc0fa60475
Jared <jared08@vt.edu>
2017-01-31 18:51:17 +0000

    user can now buy more stock

--- app/controllers/holdings_controller.rb
+++ app/controllers/holdings_controller.rb
@@ -6,8 +6,27 @@ class HoldingsController < ApplicationController
   end
 
   def create
-    debugger
-    @holding = current_user.holdings.create(holding_params.merge(:stock_id => @stock_id))
+    final_params = holding_params
+    stock_name = final_params[:stock]
+    stock = Stock.find_by(name: stock_name)
+    final_params.delete("stock") #removes the stock name retrieved by the form
+    final_params[:stock] = stock #and replaces it with actual stock
+
+    @holding = Holding.where("user_id = ? AND stock_id = ?", current_user.id, stock.id)
+    if (!@holding.empty?) #buying more
+      @holding = @holding[0] #to get array from activation record
+      new_quantity = final_params[:quantity].to_f + @holding[:quantity]
+
+      final_params[:price_at_purchase] = ((@holding[:quantity] * @holding[:price_at_purchase]) + 
+        (final_params[:quantity].to_f * stock[:current_price])) / new_quantity
+
+      final_params[:quantity] = new_quantity
+      @holding.update_attributes(final_params)
+    else #buying for the first time
+      final_params[:price_at_purchase] = stock[:current_price]
+      @holding = current_user.holdings.create(final_params)
+    end 
+
     if @holding.save
       flash[:success] = "You bought a stock!"
       redirect_to holdings_url
@@ -17,13 +36,13 @@ class HoldingsController < ApplicationController
   end
   
   def index
-    @holdings = Holding.all
+    @holdings = Holding.where(:user => current_user)
   end
 
 
   private
     def holding_params
-      params.require(:holding).permit(:stock_name, :type_of_holding, :quantity)
+      params.require(:holding).permit(:stock, :type_of_holding, :quantity)
     end
 
 end
