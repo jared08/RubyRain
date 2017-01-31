9d4a2fb506fa21e57877c8b2ee0d0cebc3a18a7c
Jared <jared08@vt.edu>
2017-01-31 19:18:39 +0000

    user can sell some or all of stock--needs error checking

--- app/controllers/holdings_controller.rb
+++ app/controllers/holdings_controller.rb
@@ -13,25 +13,44 @@ class HoldingsController < ApplicationController
     final_params[:stock] = stock #and replaces it with actual stock
 
     @holding = Holding.where("user_id = ? AND stock_id = ?", current_user.id, stock.id)
-    if (!@holding.empty?) #buying more
+    if (final_params[:type_of_holding] == 'buy')
+      if (!@holding.empty?) #buying more
+        @holding = @holding[0] #to get array from activation record
+        new_quantity = final_params[:quantity].to_i + @holding[:quantity]
+
+        final_params[:price_at_purchase] = ((@holding[:quantity] * @holding[:price_at_purchase]) + 
+          (final_params[:quantity].to_i * stock[:current_price])) / new_quantity
+
+        final_params[:quantity] = new_quantity
+        @holding.update_attributes(final_params)
+      else #buying for the first time
+        final_params[:price_at_purchase] = stock[:current_price]
+        @holding = current_user.holdings.create(final_params)
+      end 
+
+      if @holding.save
+        redirect_to holdings_url
+      else
+        render 'new'
+      end
+
+    else #selling stock
       @holding = @holding[0] #to get array from activation record
-      new_quantity = final_params[:quantity].to_f + @holding[:quantity]
+      if (@holding[:quantity] > final_params[:quantity].to_i) # only selling some
+        new_quantity = @holding[:quantity] - final_params[:quantity].to_i
+        final_params[:quantity] = new_quantity
+        @holding.update_attributes(final_params)
 
-      final_params[:price_at_purchase] = ((@holding[:quantity] * @holding[:price_at_purchase]) + 
-        (final_params[:quantity].to_f * stock[:current_price])) / new_quantity
+        if @holding.save
+          redirect_to holdings_url
+        else
+          render 'new'
+        end
 
-      final_params[:quantity] = new_quantity
-      @holding.update_attributes(final_params)
-    else #buying for the first time
-      final_params[:price_at_purchase] = stock[:current_price]
-      @holding = current_user.holdings.create(final_params)
-    end 
-
-    if @holding.save
-      flash[:success] = "You bought a stock!"
-      redirect_to holdings_url
-    else
-      render 'new'
+      else #selling all
+        Holding.find(@holding[:id]).destroy
+        redirect_to holdings_url
+      end
     end
   end
   
