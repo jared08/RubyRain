9989687952d1d95b89f4f425bdc8ecf12b4f75e7
Jared <jared08@vt.edu>
2017-02-10 21:09:56 +0000

    able to filter between completed and remaining tournaments but no able to retrieve curr stock

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,4 +1,9 @@
 class TournamentsController < ApplicationController
   def index
+    tournaments = Tournament.all
+
+    @completed_tournaments = tournaments.find_all {|x| x[:tournament_info]["IsOver"] == true}
+    @remaining_tournaments = tournaments.find_all {|x| x[:tournament_info]["IsOver"] == false}
+
   end
 end
